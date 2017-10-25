d5a7d85baab7e53413c6fd026f5224c2a26634c1
MichaelToh <ttk@sitecore.net>
2017-10-25 18:18:50 +0800

    Fix 'Tracker.Current is not initialized' error

---
+++ src/Sitecore.Support.190415/LoadEmailRenderSessionContactDecorator.cs
@@ -0,0 +1,29 @@
+﻿using Sitecore.Analytics.Pipelines.InitializeTracker;
+using Sitecore.Modules.EmailCampaign.Core.Pipelines.EnsureSessionContext;
+using Sitecore.Xdb.Configuration;
+using System;
+
+namespace Sitecore.Support.Modules.EmailCampaign.Core.Pipelines.EnsureSessionContext
+{
+    /// <summary>
+    /// Fix bug 190415
+    /// </summary>
+    public class LoadEmailRenderSessionContactDecorator
+    {
+        static LoadEmailRenderSessionContact InitLazyObject()
+        {
+            return new LoadEmailRenderSessionContact();
+        }
+
+        private Lazy<LoadEmailRenderSessionContact> lazyObject = new Lazy<LoadEmailRenderSessionContact>(InitLazyObject, true);
+
+        public void Process(InitializeTrackerArgs args)
+        {            
+            if (!XdbSettings.Enabled)
+            {
+                return;
+            }
+            lazyObject.Value.Process(args);
+        }
+    }
+}
---
+++ src/Sitecore.Support.190415/Properties/AssemblyInfo.cs
@@ -0,0 +1,6 @@
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Sitecore.Support.190415")]
+[assembly: AssemblyProduct("Sitecore.Support.190415")]
+[assembly: ComVisible(false)]
