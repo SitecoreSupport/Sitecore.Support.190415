1bacf096bfc1d5b8cf1a7555334c1a059e6c001e
MichaelToh <ttk@sitecore.net>
2017-10-26 09:47:01 +0800

    Add 2 processors to handle xDB is disabled issue

---
+++ src/Sitecore.Support.190415/DispatchRequestProcessorDecorator.cs
@@ -0,0 +1,29 @@
+﻿using Sitecore.Analytics.Pipelines.InitializeTracker;
+using Sitecore.Modules.EmailCampaign.Core.Analytics;
+using Sitecore.Xdb.Configuration;
+using System;
+
+namespace Sitecore.Support.Modules.EmailCampaign.Core.Analytics
+{
+    /// <summary>
+    /// Fix bug 190415
+    /// </summary>
+    public class DispatchRequestProcessorDecorator
+    {
+        static DispatchRequestProcessor InitLazyObject()
+        {
+            return new DispatchRequestProcessor();
+        }
+
+        private Lazy<DispatchRequestProcessor> lazyObject = new Lazy<DispatchRequestProcessor>(InitLazyObject, true);
+
+        public void Process(InitializeTrackerArgs args)
+        {
+            if (!XdbSettings.Enabled)
+            {
+                return;
+            }
+            lazyObject.Value.Process(args);
+        }
+    }
+}
---
+++ src/Sitecore.Support.190415/MarkEmailRenderSessionTransientDecorator.cs
@@ -0,0 +1,29 @@
+﻿using Sitecore.Analytics.Pipelines.InitializeTracker;
+using Sitecore.Modules.EmailCampaign.Core.Pipelines.InitializeTracker;
+using Sitecore.Xdb.Configuration;
+using System;
+
+namespace Sitecore.Support.Modules.EmailCampaign.Core.Pipelines.InitializeTracker
+{
+    /// <summary>
+    /// Fix bug 190415
+    /// </summary>
+    public class MarkEmailRenderSessionTransientDecorator
+    {
+        static MarkEmailRenderSessionTransient InitLazyObject()
+        {
+            return new MarkEmailRenderSessionTransient();
+        }
+
+        private Lazy<MarkEmailRenderSessionTransient> lazyObject = new Lazy<MarkEmailRenderSessionTransient>(InitLazyObject, true);
+
+        public void Process(InitializeTrackerArgs args)
+        {
+            if (!XdbSettings.Enabled)
+            {
+                return;
+            }
+            lazyObject.Value.Process(args);
+        }
+    }
+}
